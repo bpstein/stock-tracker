b351039e66663ef9c5277e0204191c89975d831e
Ben Stein <bumstain@gmail.com>
2015-12-09 00:23:45 +0000

    Configured the stocks and stock view templates

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -41,16 +41,27 @@ angular.module('stock-tracker.controllers', [])
   };
 })
 
-.controller('MyStocksCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
+.controller('MyStocksCtrl', ['$scope',
+  function($scope) {
+  $scope.myStocksArray = [
+    { ticker: 'AAPL'},
+    { ticker: 'GPRO'},
+    { ticker: 'FB'},
+    { ticker: 'NFLX'},
+    { ticker: 'TSLA'},
+    { ticker: 'BRK-A'},
+    { ticker: 'INTC'},
+    { ticker: 'MSFD'},
+    { ticker: 'GE'},
+    { ticker: 'C'},
+    { ticker: 'T'},
+    { ticker: 'BAC'},
   ];
-})
+}])
 
-.controller('StockCtrl', function($scope, $stateParams) {
-});
+.controller('StockCtrl', ['$scope', '$stateParams',
+  function($scope, $stateParams) {
+
+    $scope.ticker = $stateParams.stockTicker;
+
+}]);
